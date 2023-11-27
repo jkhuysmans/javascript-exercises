8ecd8c75b82ec9d236e95afe6a510690730da0e3
jkhuysmans <149975556+jkhuysmans@users.noreply.github.com>
2023-11-27 14:42:15 +0100

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInput) {
+  let temp = (tempInput - 32 ) * 5 / 9;
+  return parseFloat(temp.toFixed(1));
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInput) {
+  let temp = (tempInput * 9/5 + 32);
+  return parseFloat(temp.toFixed(1));
 };
 
 // Do not edit below this line
